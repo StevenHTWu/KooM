bd540c4171bf74f456ed00352ec50dd016c65781
Wei Thang Chai <weithangchai@WeiThangs-MacBook-Pro.local>
2021-02-20 21:26:10 -0800

    frontend done, signup, login, password reset, logout, update profile

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,38 @@
 import React from "react";
 import "./App.css";
-import TempSocket from "./components/TempSocket";
+//import TempSocket from "./components/TempSocket";
 import Signup from "./pages/SignUp";
+import Dashboard from "./pages/Dashboard";
+import Login from "./pages/Login";
 import { Container } from "react-bootstrap";
 import { AuthProvider } from "./contexts/AuthContext";
+import { BrowserRouter as Router, Switch, Route} from "react-router-dom"
+import PrivateRoute from "./pages/PrivateRoute"
+import ForgotPassword from "./pages/ForgotPassword";
+import UpdateProfile from "./pages/UpdateProfile";
 
 function App() {
   return (
-    <AuthProvider>
+    
       <Container
         className="d-flex align-items-center justify-content-center"
         style={{ minHeight: "100vh" }}
       >
         <div className="w-100" style={{ maxWidth: "400px" }}>
-          <Signup />
+          <Router>
+            <AuthProvider>
+              <Switch>
+                <PrivateRoute exact path="/" component={Dashboard} />
+                <PrivateRoute path="/update-profile" component={UpdateProfile} />
+                <Route path="/signup" component={Signup} />
+                <Route path="/login" component={Login} />
+                <Route path="/forgot-password" component={ForgotPassword} />
+              </Switch>
+            </AuthProvider>
+          </Router>
         </div>
       </Container>
-    </AuthProvider>
+    
   );
 }
 
